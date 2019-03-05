6a7d7b7c73ce3d2fed995ddcd4af2530f68275eb
matthew cook <mcook83@gmail.com>
2019-03-05 20:10:19 +0000

    include x-alt-description

--- django_ical/feedgenerator.py
+++ django_ical/feedgenerator.py
@@ -68,6 +68,7 @@ ITEM_EVENT_FIELD_MAP = (
     ('rdate',               'rdate'),
     ('exdate',              'exdate'),
     ('status',              'status'),
+    ('html_description',    'x-alt-desc'),
 )
 
 
